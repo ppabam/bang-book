509785095fa36d8e049551d07c5f8940dc1a84f2
ppabam <tom@diginori.com>
2025-05-14 14:42:59 +0900

    openGraph img

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BBook",
+  description: "회의실 방 예약 시스템",
+  openGraph: {
+    title: "회의실 방 예약 시스템",
+    description: "회의실 방 예약 시스템",
+    images: ["https://bang-book.vercel.app/og.jpg"],
+  },
+};
 
 export default function RootLayout({
   children,
